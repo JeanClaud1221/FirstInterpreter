81ee5c16577173e26cbaecb18173b328a9bb08db
jeanclaud1221 <jeannclaudkumm@gmail.com>
2024-07-04 19:52:40 +0200

    Added parentheses functionality

--- interpreter.py
+++ interpreter.py
@@ -1,30 +1,21 @@
-EOF,INTEGER,PLUS,MINUS,MULT,DIV="EOF","INTEGER","PLUS","MINUS","MULT","DIV"
+EOF,PLUS,MINUS,MULT,DIV,INTEGER,LPAREN,RPAREN="EOF","PLUS","MINUS","MULT","DIV","INTEGER","LPAREN","RPAREN"
 class Token():
     def __init__(self,value,type):
         self.value=value
         self.type=type
-    def __repr__(self):
+    def __str__(self) -> str:
+        return f"Value:{self.value},Type:{self.type}"
+    def __repr__(self) -> str:
         return self.__str__()
-    def __str__(self):
-        return f"Value:{self.value} Type:{self.type}"
-
+    
 class Lexer():
     def __init__(self,text:str):
         self.text=text
-        self.pos=0
-        self.current_char=self.text[self.pos]
-        # self.current_token=None
-    def error(self):
-        raise Exception("Lexer error")
-    def advance(self):
-        self.pos+=1
-        if self.pos>len(self.text)-1:
-            self.current_char=None
-        else:
-            self.current_char=self.text[self.pos]
-    def skip_white_space(self):
-        while self.current_char is not None and self.current_char.isspace():
-            self.advance()
+        self.current_pos=0
+        self.current_char=self.text[self.current_pos]
+
+    def error(self,err):
+        raise Exception(err)
     
     def integer(self):
         result=""
@@ -32,16 +23,37 @@ class Lexer():
             result+=self.current_char
             self.advance()
         return int(result)
-    def get_next_token(self):
 
+    def advance(self):
+        self.current_pos+=1
+        if self.current_pos>len(self.text)-1:
+            self.current_char=None
+        else:
+            self.current_char=self.text[self.current_pos]
+    
+    def skip_space(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def get_next_token(self):
         while self.current_char is not None:
 
             if self.current_char.isspace():
-                self.skip_white_space()
+                self.skip_space()
                 continue
 
+            if self.current_char.isdigit():
+                token=Token(self.integer(),INTEGER)
+                # self.advance()
+                return token
+            
             if self.current_char=="+":
                 token=Token("+",PLUS)
+                self.advance()
+                return token
+            
+            if self.current_char=="-":
+                token=Token("-",MINUS)
                 self.advance()
                 return token
             
@@ -55,37 +67,41 @@ class Lexer():
                 self.advance()
                 return token
             
-            if self.current_char=="-":
-                token=Token("-",MINUS)
+            if self.current_char=="(":
+                token=Token("(",LPAREN)
                 self.advance()
                 return token
             
-            if self.current_char.isdigit():
-                token=Token(self.integer(),INTEGER)
-                # self.advance()
+            if self.current_char==")":
+                token=Token(")",RPAREN)
+                self.advance()
                 return token
             
-        return Token("EOF",EOF)
-
-class Intepreter():
+        return Token(None,EOF)
+        
+class Interpreter():
     def __init__(self,lexer:Lexer):
         self.lexer=lexer
         self.current_token=self.lexer.get_next_token()
-
-    def error(self,err:str):
+    def error(self,err):
         raise Exception(err)
-            
     def eat(self,token_type):
-        if self.current_token.type==token_type:
+        if token_type==self.current_token.type:
             self.current_token=self.lexer.get_next_token()
         else:
-            self.error("Error token type doesn't match")
-    
+            self.error("Error eating token")
+
     def factor(self):
         token=self.current_token
-        self.eat(INTEGER)
-        return token.value
-    
+        if token.type==INTEGER:
+            self.eat(INTEGER)
+            return token.value
+        if token.type==LPAREN:
+            self.eat(LPAREN)
+            result=self.expr()
+            self.eat(RPAREN)
+            return result
+
     def term(self):
         result=self.factor()
         while self.current_token.type in (MULT,DIV):
@@ -97,12 +113,10 @@ class Intepreter():
                 self.eat(DIV)
                 result=result/self.factor()
         return result
-
     def expr(self):
         result=self.term()
         while self.current_token.type in (PLUS,MINUS):
             op=self.current_token
-
             if op.type==PLUS:
                 self.eat(PLUS)
                 result+=self.term()
@@ -110,25 +124,10 @@ class Intepreter():
                 self.eat(MINUS)
                 result-=self.term()
         return result
-        # op=self.current_token
-        # if op.type==PLUS:
-        #     self.eat(PLUS)
-        # elif op.type==MINUS:
-        #     self.eat(MINUS)
-
-        # right=self.current_token
-        # self.eat(INTEGER)
-
-        # if op.type==PLUS:
-        #     return left.value+right.value
-        # else:
-        #     return left.value-right.value
-            
-def main():
-    exp=input("calc>")
-    lexer=Lexer(exp)
-    inter=Intepreter(lexer)
-    result=inter.expr()
-    print(result)
 if __name__=="__main__":
-    main()
+    inner=input("calc>")
+    lexer=Lexer(inner)
+    result=Interpreter(lexer)
+    print(result.expr())
+
+
